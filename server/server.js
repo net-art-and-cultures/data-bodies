472d7285400e114ea8058875352fd7b222402e1b
Jieun LEE <j.i.v.e.l.y@Jieuns-MacBook-Pro.local>
2019-12-05 01:20:09 -0600

    change server.js

--- server/server.js
+++ server/server.js
@@ -8,6 +8,9 @@ const staticFiles = express.static(`${__dirname}/www`)
 
 app.use(staticFiles)
 
+app.post(/api/image-uploaded)
+app.get(/api/images)
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}, CTRL + C to shutdown`)
 })
