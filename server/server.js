aab751afc619034ad1c046eb95ca5c9d5ef351a7
Dain Jung <dainjung@Dains-MacBook-Pro.local>
2019-12-08 13:11:47 -0600

    dynamic

--- server/server.js
+++ server/server.js
@@ -11,3 +11,7 @@ app.use(staticFiles)
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}, CTRL + C to shutdown`)
 })
+
+app.get('https://raw.githubusercontent.com/net-art-and-cultures/syllabus-and-notes/master/notes/img-test.json', (request, respond) => {
+  res.send('hey')
+})
