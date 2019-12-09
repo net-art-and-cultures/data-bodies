a507f72af256ba52005d55d287b4411c51e9d6be
Dain Jung <dainjung@Dains-MacBook-Pro.local>
2019-12-08 20:13:29 -0600

    newPost

--- server/server.js
+++ server/server.js
@@ -6,12 +6,13 @@ const port = Number(process.argv[2]) || 8000
 
 const staticFiles = express.static(`${__dirname}/www`)
 
+
 app.use(staticFiles)
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}, CTRL + C to shutdown`)
 })
 
-app.get('https://raw.githubusercontent.com/net-art-and-cultures/syllabus-and-notes/master/notes/img-test.json', (request, respond) => {
+app.get('https://raw.githubusercontent.com/net-art-and-cultures/syllabus-and-notes/master/notes/img-test.json', (request, response) => {
   res.send('hey')
 })
